172cc43171855f92fbc6deb848f4a1ef65fb54c6
MuhammadHafizhMaulana <maulanahafizh146@gmail.com>
2025-03-26 11:32:32 +0700

    mengganti koneksi ke cloud

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("note", "root","", {
-  host: "localhost",
+const db = new Sequelize("note", "root","admin123", {
+  host: "34.27.42.159",
   dialect: "mysql",
 });
 
